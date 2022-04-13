14d338dab43fb22540b0d693f1d774150b5bd5dc
MaxHoecker <hoecker.max@gmail.com>
2022-04-13 19:40:11 -0400

    hopefully setting up tls, please...

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,5 +1,5 @@
-#sudo systemctl stop nginx
+sudo systemctl stop nginx
 cp ./max-hoecker.conf /etc/nginx/conf.d/
 cp ./nginx.conf /etc/nginx/
-#sudo systemctl start nginx
-#npm run-script build
+sudo systemctl start nginx
+npm run-script build
