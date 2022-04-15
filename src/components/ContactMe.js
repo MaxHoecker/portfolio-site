d738693179c43d2d8491f5d9bac9b611a575b3b0
MaxHoecker <hoecker.max@gmail.com>
2022-04-15 00:09:22 -0400

    refactored Contact Me

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,45 +1,58 @@
+import contactMe from "./../siteText/ContactMe.json"
+
 export default function ContactMe() {
 
+    const wrapperCss = "bg-backgroundColor m-4 p-6 rounded-2xl text-primary whitespace-pre-wrap";
     const contactCss = "text-primary m-4";
     const headerCss = "text-secondary text-xl";
     const contactInfoLinkCss = "ml-4 hover:text-secondary";
     const contactInfoNoLinkCss = "ml-4";
 
+    let contactItems = []
+
+    const contactItem = (props) => {
+        let addressCss = props.link ? contactInfoLinkCss : contactInfoNoLinkCss;
+        let contents = []
+
+        props.content.forEach((address) => {
+            if (props.link) {
+                contents.push(
+                    <a className={addressCss} href={address} target="_blank" rel="noopener noreferrer">
+                        {address}
+                        <br/>
+                    </a>
+                )
+            }
+            else {
+                contents.push(
+                    <p className={addressCss}>
+                        {address}
+                        <br/>
+                    </p>
+                )
+            }
+        })
+
+        return (
+            <div className={contactCss}>
+                <h2 className={headerCss}>
+                    {props.header}
+                </h2>
+                {contents}
+            </div>
+        )
+    }
+
+    for (const value of Object.values(contactMe)) {
+        contactItems.push(contactItem(value))
+    }
+
     return (
-        <div className={"bg-backgroundColor m-4 p-6 rounded-2xl text-primary whitespace-pre-wrap"}>
+        <div className={wrapperCss}>
             <p>
                 Feel free to reach out to me about anything
             </p>
-
-            <div className={contactCss}>
-                <h2 className={headerCss}>
-                    Email:
-                </h2>
-                <p className={contactInfoNoLinkCss}>
-                    mjh9131@rit.edu (preferred) <br/>
-                    hoecker.max@gmail.com
-                </p>
-            </div>
-
-            <div className={contactCss}>
-                <h2 className={headerCss}>
-                    Linkedin:
-                </h2>
-                <a className={contactInfoLinkCss} href="https://linkedin.com/in/MaxHoecker">
-                    https://github.com/MaxHoecker
-                </a>
-            </div>
-
-            <div className={contactCss}>
-                <h2 className={headerCss}>
-                    Github:
-                </h2>
-                <a className={contactInfoLinkCss} href="https://github.com/MaxHoecker">
-                    https://github.com/MaxHoecker
-                </a>
-            </div>
-
-
+            {contactItems}
         </div>
     );
 }
